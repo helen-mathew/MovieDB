8129e7964c77b170471ab291d14c908618416215
Helen Mathew <helenn.mathew@gmail.com>
2023-04-11 13:46:37 -0400

    add mongo string

--- projectserver.js
+++ projectserver.js
@@ -13,7 +13,8 @@ require("dotenv").config();
 const session = require("express-session");
 const MongoDBStore = require("connect-mongodb-session")(session);
 
-const mongo_uri = process.env.MONGODB_URI;
+const mongo_uri =
+    "mongodb+srv://helen:RVayMVPaFrWYWQA1@moviedb.b6utl.mongodb.net/moviedb?retryWrites=true&w=majority";
 
 //console.log(mongo_uri);
 const store = new MongoDBStore({
